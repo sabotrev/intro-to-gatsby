3fc4d1a93056916a13d0d5861db074a8177c4bf1
Trevor Sabo <sabotrev@gmail.com>
2019-09-21 17:12:49 -0400

    Added emotion css dependency; Added layout & header components; Added home & about pages

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,8 +4,8 @@ import Layout from "../components/layout"
 
 export default () => (
   <Layout>
-    <h1>Home</h1>
-    <p>Hello</p>
-    <Link to="/about/">Learn about me &rarr;</Link>
+    <h1>About Me</h1>
+    <p>This is my personal website.</p>
+    <Link to="/">&larr; back to home</Link>
   </Layout>
 )
