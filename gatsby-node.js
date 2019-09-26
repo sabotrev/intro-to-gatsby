cc984165310b4e8e3d4249569bc952b670dd6de5
Trevor Sabo <sabotrev@gmail.com>
2019-09-25 21:53:58 -0400

    Added post template Added use posts hook Added read link and post preview components Added blog markdown files Added gatsby-node.js for generating pages

---
+++ gatsby-node.js
@@ -0,0 +1,29 @@
+exports.createPages = async ({ actions, graphql, reporter }) => {
+  const results = await graphql(`
+    query {
+      allMdx {
+        nodes {
+          frontmatter {
+            slug
+          }
+        }
+      }
+    }
+  `)
+
+  if (results.errors) {
+    reporter.panic("failed to create posts", results.errors)
+  }
+
+  const posts = results.data.allMdx.nodes
+
+  posts.forEach(post => {
+    actions.createPage({
+      path: post.frontmatter.slug,
+      component: require.resolve("./src/templates/post.js"),
+      context: {
+        slug: post.frontmatter.slug,
+      },
+    })
+  })
+}
